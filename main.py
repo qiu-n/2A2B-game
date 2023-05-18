1dd0bad47d6d66bc09c38b36be161aba52c913be
alanqiu <alan928166@gmail.com>
2023-05-18 12:45:09 +0800

    2A2B game

---
+++ main.py
@@ -0,0 +1,42 @@
+import gamefunction as gf
+
+
+count = 0
+array = gf.arrayget()
+gf.gamestart()
+
+while (1):
+    ans = gf.arrayinput()
+    if (gf.arraycheck(ans) == 1):
+        print('\n', gf.Dividingline, '\n',
+              gf.Errorsign, '\n', gf.Dividingline,)
+        print(gf.tryagain, end=" ")
+        continue
+    count += 1
+    cheak = gf.Anscheak(array, ans)
+    if (cheak == 1):
+        break
+    elif (count == 3):
+        print(gf.encourage1)
+    elif (count == 6):
+        print(gf.encourage2)
+    elif (count == 9):
+        print(gf.encourage3)
+    elif (count == 12):
+        print(gf.encourage4)
+    elif (count == 15):
+        print(gf.encourage5)
+    elif (count == 18):
+        print(gf.encourage6)
+    elif (count == 21):
+        print(gf.encourage7)
+    elif (count == 24):
+        print(gf.encourage8)
+    elif (count == 27):
+        print(gf.encourage9)
+    elif (count == 30):
+        print(gf.encourage10)
+    elif (count > 30):
+        print(gf.encourage11)
+    print(gf.Sorry, end=" ")
+print(gf.Correct)
